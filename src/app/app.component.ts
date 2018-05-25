c65fda30a3efe407305df491422fda73b47bc0a4
stan <stann84@hotmail.com>
2018-05-25 03:05:41 +0200

    vide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyAjJqj6p5061e7SBUE6e_07x_gfzH2LfUI",
+      authDomain: "blog-7bd4e.firebaseapp.com",
+      databaseURL: "https://blog-7bd4e.firebaseio.com",
+      projectId: "blog-7bd4e",
+      storageBucket: "blog-7bd4e.appspot.com",
+      messagingSenderId: "1097932278053"
+    };
+    firebase.initializeApp(config);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { HeaderComponent } from './header/header.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import { PostFormComponent } from './post-form/post-form.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
+const appRoutes: Routes = [
+  {path: 'posts' , component: PostListComponent},
+  {path: 'new' , component: PostFormComponent},
+  {path: '' , component: PostListComponent},
+  {path: 'not-found' , component: FourOhFourComponent},
+  {path: '**' , redirectTo: '/not-found'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    HeaderComponent,
+    PostFormComponent,
+    FourOhFourComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
